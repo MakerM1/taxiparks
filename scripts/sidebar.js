48720ef4e2cc3d1e2018aa561518753c22029b28
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-01-21 23:30:55 +0400

    phone nav

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,14 +1,24 @@
 const hamburgerMenu = document.getElementById('hamburgerMenu')
 const sidebar = document.getElementById('sideBar')
+const quitButton = document.querySelector('.aside-profile__quit')
 
-document.addEventListener('load', () => {
-    if (document.body.clientWidth <= 1030) {
-        sidebar.classList.add('closed')
-        console.log('hi')
-    } 
-})
+if (document.body.clientWidth <= 1030 && document.body.clientWidth > 600) {
+    sidebar.classList.add('closed')
+    console.log('hi')
+} 
 
-hamburgerMenu.addEventListener('click', () => {
-    sidebar.classList.toggle('closed')
-    console.log('click')
-})
+if (document.body.clientWidth > 600) {
+    hamburgerMenu.addEventListener('click', () => {
+        sidebar.classList.toggle('closed')
+        console.log('click')
+    })
+} else if (document.body.clientWidth <= 600) {
+    hamburgerMenu.addEventListener('click', () => {
+        sidebar.classList.toggle('active')
+        console.log('click')
+    })
+
+    quitButton.addEventListener('click', () => {
+        sidebar.classList.remove('active')
+    })
+}
