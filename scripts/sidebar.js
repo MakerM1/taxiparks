8fe210b244b58c8371d4868721cc018b9bd68802
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-01-22 19:10:17 +0400

    v1

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,17 +2,12 @@ const hamburgerMenu = document.getElementById('hamburgerMenu')
 const sidebar = document.getElementById('sideBar')
 const quitButton = document.querySelector('.aside-profile__quit')
 
-if (document.body.clientWidth <= 1030 && document.body.clientWidth > 600) {
-    sidebar.classList.add('closed')
-    console.log('hi')
-} 
-
-if (document.body.clientWidth > 600) {
+if (document.body.clientWidth > 1030) {
     hamburgerMenu.addEventListener('click', () => {
         sidebar.classList.toggle('closed')
         console.log('click')
     })
-} else if (document.body.clientWidth <= 600) {
+} else if (document.body.clientWidth <= 1030) {
     hamburgerMenu.addEventListener('click', () => {
         sidebar.classList.toggle('active')
         console.log('click')
